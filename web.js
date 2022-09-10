493031db29042ee5fdd2675de38a5b8eff603110
Davis <108025333+ZagrywacZ@users.noreply.github.com>
2022-09-10 19:48:05 +0200

    Update 1.0

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
